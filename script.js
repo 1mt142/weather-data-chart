5425db6ff24cfd099a772ae43b5f8ca35735e797
Imtiaz Khandoker <imtiazkhandoker@Imtiazs-MacBook.local>
2023-08-14 01:25:32 +0600

    display the 400 error message

--- script.js
+++ script.js
@@ -68,9 +68,16 @@ async function fetchWeatherData(start_date, end_date) {
 
   try {
     const response = await fetch(apiUrl);
+
+    if (response.status === 400) {
+      const errorData = await response.json();
+      alert(errorData.reason);
+    }
+
     if (!response.ok) {
       throw new Error("API Error");
     }
+
     const data = await response.json();
     resp = data;
     displayWeatherTable(data);
